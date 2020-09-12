06857857d4745bb30e0ca2fc6cd84a8b900c00c2
Wilson Ruan <wilsonruan23@gmail.com>
2020-09-12 19:25:18 -0400

    Submit button pushs to main page.

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
+  var toMainPage = $("form")
+  var userNameInput = $("input#fullName");
+
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+
+  toMainPage.on("submit", function(event) {
+    event.preventDefault();
+    console.log(userNameInput)
+    towardMainPage();
+  })
+
+  function towardMainPage () {
+    window.location.replace("/mainpage");
+  }
 });
